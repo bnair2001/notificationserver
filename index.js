32dee9c2fe260e3db739b2b03b123ed52f1b580b
Bharath <bharathrajeevnair@gmail.com>
2019-07-26 21:54:46 +0930

    initial commit

---
+++ index.js
@@ -0,0 +1,57 @@
+var firebase = require('firebase-admin');
+var request = require('request');
+
+var API_KEY = "AIzaSyDdpe6ukDL-owVlyIYxWvh4gtCr_qU_U38";
+
+var serviceAccount = require("./disastercomms-56253-firebase-adminsdk-99ipf-1c787261cb.json");
+
+firebase.initializeApp({
+  credential: firebase.credential.cert(serviceAccount),
+  databaseURL: "https://disastercomms-56253.firebaseio.com"
+});
+
+ref = firebase.database().ref();
+
+function listenForNotificationRequests() {
+  var requests = ref.child('notificationRequests');
+  requests.on('child_added', function(requestSnapshot) {
+    var request = requestSnapshot.val();
+    sendNotificationToUser(
+      request.username, 
+      request.message,
+      function() {
+        requestSnapshot.ref.remove();
+      }
+    );
+  }, function(error) {
+    console.error(error);
+  });
+};
+
+function sendNotificationToUser(username, message, onSuccess) {
+  request({
+    url: 'https://fcm.googleapis.com/fcm/send',
+    method: 'POST',
+    headers: {
+      'Content-Type' :' application/json',
+      'Authorization': 'key='+API_KEY
+    },
+    body: JSON.stringify({
+      notification: {
+        title: message
+      },
+      to : '/topics'
+    })
+  }, function(error, response, body) {
+    if (error) { console.error(error); }
+    else if (response.statusCode >= 400) { 
+      console.error('HTTP Error: '+response.statusCode+' - '+response.statusMessage); 
+    }
+    else {
+      onSuccess();
+    }
+  });
+}
+
+// start listening
+listenForNotificationRequests();
