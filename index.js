6cf2998187daa26773e7698a8b70fb8ddb443ae5
Bharath Nair <bharath.rajeevnair@student.adelaide.edu.au>
2019-07-26 22:11:10 +0930

    minor changes

--- index.js
+++ index.js
@@ -1,57 +1,121 @@
-var firebase = require('firebase-admin');
-var request = require('request');
+var express = require('express');
+var app = express();
 
-var API_KEY = "AIzaSyDdpe6ukDL-owVlyIYxWvh4gtCr_qU_U38";
+app.set('port', (process.env.PORT || 5000));
 
-var serviceAccount = require("./disastercomms-56253-firebase-adminsdk-99ipf-1c787261cb.json");
-
-firebase.initializeApp({
-  credential: firebase.credential.cert(serviceAccount),
-  databaseURL: "https://disastercomms-56253.firebaseio.com"
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
 
-ref = firebase.database().ref();
+var firebase = require("firebase");
+var request = require('request');
 
-function listenForNotificationRequests() {
-  var requests = ref.child('notificationRequests');
-  requests.on('child_added', function(requestSnapshot) {
-    var request = requestSnapshot.val();
-    sendNotificationToUser(
-      request.username, 
-      request.message,
-      function() {
-        requestSnapshot.ref.remove();
-      }
-    );
-  }, function(error) {
-    console.error(error);
-  });
-};
+// Initialize the app with a service account, granting admin privileges
+firebase.initializeApp({
+  databaseURL: "https://disastercomms-56253.firebaseio.com",
+  serviceAccount: "./disastercomms-56253-firebase-adminsdk-99ipf-1c787261cb.json",
+});
 
-function sendNotificationToUser(username, message, onSuccess) {
-  request({
-    url: 'https://fcm.googleapis.com/fcm/send',
-    method: 'POST',
-    headers: {
-      'Content-Type' :' application/json',
-      'Authorization': 'key='+API_KEY
-    },
-    body: JSON.stringify({
-      notification: {
-        title: message
-      },
-      to : '/topics'
-    })
-  }, function(error, response, body) {
-    if (error) { console.error(error); }
-    else if (response.statusCode >= 400) { 
-      console.error('HTTP Error: '+response.statusCode+' - '+response.statusMessage); 
+console.log("Firebase initialization passed");
+
+// As an admin, the app has access to read and write all data, regardless of Security Rules
+var db = firebase.database();
+var ref = db.ref("notifications");
+
+// This callback function is needed to detect changes in the number of attemps.
+// Note that a proper exponential backoff algorithm would work much better,
+// but just trying to send the notification 10 times is decent for testing / beta launch
+ref.orderByChild("sent").equalTo(false).on("child_changed", function(snapshot) {
+  console.log("Snapshot recieved (child_changed)");
+
+  handleNotificationSnapshot(snapshot)
+});
+
+// This callback function is needed to detect additions to notification/ in the Firebase databse
+ref.orderByChild("sent").equalTo(false).on("child_added", function(snapshot) {
+  console.log("Snapshot recieved (child_added)");
+
+  handleNotificationSnapshot(snapshot)
+});
+
+function handleNotificationSnapshot(snapshot) {
+  if (snapshot.val() != null) {   // Make sure that the snapshot is not empty
+
+    // Log the number of send attemps
+    console.log("Number of attemps " + snapshot.val()["attempts"])
+
+    // Double check that the notification has not been sent, and that the number of attemps is less than 10
+    if (snapshot.val()["sent"] === false && snapshot.val()["attempts"] < 10) {
+
+      console.log("Muts get notification ids");
+
+      // Create a reference to the notification ids (tokens) in the Firebase database
+      var userNotificationIDRef = db.ref("notification_ids/" + snapshot.val()["recipientUID"]);
+
+      // Retrieve the notification ids
+      userNotificationIDRef.once("value", function(notificationSnapshot) {
+
+        if (notificationSnapshot.val() != null) { // Make sure that the snapshot is not empty
+
+          console.log("Notification ids recieved");
+
+          // Send the notification
+          sendNotification(notificationSnapshot.val(), snapshot.val()["message"], snapshot.val()["key"], snapshot.val()["attempts"])
+        }
+        else {
+          console.log("No notification ids registered");
+        }
+      });
+
     }
-    else {
-      onSuccess();
-    }
-  });
+  }
 }
 
-// start listening
-listenForNotificationRequests();
+// This is a function which sends notifications to multiple devices
+function sendNotification(notificationID, message, key, attempts) {
+
+  console.log("Sending notification");
+
+  request(
+    {
+      method: 'POST',
+      uri: 'https://fcm.googleapis.com/fcm/send',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'key=CLOUD MESSAGING SERVER KEY'
+      },
+      body: JSON.stringify({
+        "registration_ids": notificationID, // Ask me for this when u deploy
+        "priority": "high", // Change this value for different behavior on devices
+        "notification" : {
+          "body" : "Disaster Alert !!",
+          "title": "DisCom",
+          "sound": "default",
+          "badge": 0
+        }
+      })
+    },
+    function (error, response, body) {
+      if(response.statusCode == 200){
+
+        console.log('Success')
+
+        // Create a reference to the notification in the Firebase database
+        var notificationRef = db.ref("notifications/" + key);
+
+        // Set "sent" to true to avoid the notification being sent more than once
+        notificationRef.child("sent").set(true);
+
+        // Increment the "attempts"
+        notificationRef.child("attempts").set(attempts+1);
+      } else {
+        console.log('error: '+ response.statusCode)
+
+        // Create a reference to the notifications in the Firebase database
+        var notificationRef = db.ref("notifications/" + key);
+
+        // Increment the "attempts". Since "sent" is still false "child_changed" will be called
+        notificationRef.child("attempts").set(attempts+1);
+      }
+    }
+  )
